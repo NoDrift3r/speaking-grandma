21860c0560150a5a069501f9e220d1809105dec8
Jesus <jose.jesus16@gmail.com>
2021-03-04 21:37:51 -0500

    commit tree aa6e1b3553aa622557c8a56428f9edb3aa3de519

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,48 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+#if phrase does this phrase-upcase
+#return this "i love you too pumpkin"
+#elsif phrase-downcase
+#return this "huh , speak up sonny"
+#elsif phrase
+#return "no not since 1938"
+
+
+# if  phrase == "I LOVE YOU GRANDMA!"
+#   return “I LOVE YOU TOO PUMPKIN!”
+# elsif  phrase == phrase.upcase
+#  “NO, NOT SINCE 1938!”
+   
+#     #"HUH?! SPEAK UP, SONNY!"
+# else 
+#     "HUH?! SPEAK UP, SONNY!"
+#    # puts  "NO, NOT SINCE 1938"
+#  end
+# end
+
+
+# if  phrase == "I LOVE YOU GRANDMA!"
+#   “I LOVE YOU TOO PUMPKIN!”
+# elsif  phrase == phrase.upcase
+#  “NO, NOT SINCE 1938!”
+#    # binding.pry
+#    #  return "HUH?! SPEAK UP, SONNY!"
+# else 
+#     "HUH?! SPEAK UP, SONNY!"
+#    # puts  "NO, NOT SINCE 1938"
+#  end
+# end
+
+
+
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase()
+      return "NO, NOT SINCE 1938!"
+  else phrase
+      return "HUH?! SPEAK UP, SONNY!"
+  end
+end
